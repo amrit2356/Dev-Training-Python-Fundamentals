a4c207f5ee3c559d32b1d7c8880e9b704cf9297e
Amritanshu Pandey <amritanshupandey.meritocrat@gmail.com>
2020-12-24 13:25:16 +0530

    abstract class

--- 01_Python_OOP/01_0_OOP_Basics/12_OOP_abstract_class_method.py
+++ 01_Python_OOP/01_0_OOP_Basics/12_OOP_abstract_class_method.py
@@ -1,4 +1,17 @@
 """
 Implementation of Abstract class in python 
 """
+from abc import ABC, abstractmethod
 
+class Computer(ABC):
+    @abstractmethod
+    def process(self):
+        pass
+
+class Laptop(Computer):
+    def process(self):
+        print("its running...")
+
+
+com = Laptop()
+com.process()
