a3c38d02ff54cec4f1f249d4f664fad2542a78d6
Amritanshu Pandey <amritanshupandey.meritocrat@gmail.com>
2020-12-28 10:11:14 +0530

    Decorators

--- 05_Python_Decorators/01_Decorators_intro.py
+++ 05_Python_Decorators/01_Decorators_intro.py
@@ -2,10 +2,22 @@
 Implementation of Decorators in Python
 
 """
-
-def div(a,b):
+@
+def div(a,b):        
     print(a/b)
 
 
+def smart_div(func):
+    def inner(a,b):
+        if(a<b):
+            a,b = b,a
 
-div(2,4)
+        return func(a,b)
+    
+    return inner
+
+        
+
+
+div_new = smart_div(div)
+div_new(4,2)
