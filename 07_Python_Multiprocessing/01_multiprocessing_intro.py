4f500ab8e35309e2b8df27e9ac6a20ca9f8de3e3
Amritanshu Pandey <amritanshupandey.meritocrat@gmail.com>
2021-01-04 16:17:14 +0530

    multiprocessing code using python

---
+++ 07_Python_Multiprocessing/01_multiprocessing_intro.py
@@ -0,0 +1,37 @@
+"""
+Implementation of MultiProcessing in Python
+"""
+
+import time
+import multiprocessing
+
+start = time.perf_counter()
+
+
+def do_something(seconds):
+    print(f'Sleeping for {seconds} sec')
+    time.sleep(1)
+    print('Done Sleeping')
+
+
+# p1 = multiprocessing.Process(target=do_something)
+# p2 = multiprocessing.Process(target=do_something)
+
+# p1.start()
+# p2.start()
+
+# p1.join()
+# p2.join()
+
+processes = []
+for _ in range(10):
+    p = multiprocessing.Process(target=do_something, args=[1.5])
+    p.start()
+    processes.append(p)
+
+for process in processes:
+    process.join()
+
+
+finish = time.perf_counter()
+print(f'Finished in {round(finish-start, 2)} second(s)')
---
+++ 07_Python_Multiprocessing/02_multiprocessing_concurrent.py
@@ -0,0 +1,20 @@
+import time
+import concurrent.futures
+start = time.perf_counter()
+
+
+def do_something(seconds):
+    print(f'Sleeping for {seconds} sec')
+    time.sleep(seconds)
+    return f'Done Sleeping in {seconds}'
+
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    secs = [5, 4, 3, 2, 1]
+    results = [executor.submit(do_something, sec) for sec in secs]
+    for f in concurrent.futures.as_completed(results):
+        print(f.result())
+
+
+finish = time.perf_counter()
+print(f'Finished in {round(finish-start, 2)} second(s)')
