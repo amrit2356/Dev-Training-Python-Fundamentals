843d61fe4305d7ef807c1d800360c7b3fbb9fe65
Amritanshu Pandey <amritanshupandey.meritocrat@gmail.com>
2021-01-06 15:11:12 +0530

    Decorator code linted

--- 05_Python_Decorators/01_Decorators_intro.py
+++ 05_Python_Decorators/01_Decorators_intro.py
@@ -2,22 +2,26 @@
 Implementation of Decorators in Python
 
 """
-def decorator_function(original_function):
+
+
+def decorator_function(orig_func):
     def wrapper_function():
-        print('Wrapper Executed this before: {}()'.format(original_function.__name__))
-        return original_function()
-    
+        print('Wrapper Executed this before: {}()'.format(orig_func.__name__))
+        return orig_func()
     return wrapper_function
 
-#Syntax 1 for creation of Decorators
+# Syntax 1 for creation of Decorators
 # def display():
 #     print('display function Ran')
 # decorated_display = decorator_function(display)
 # decorated_display()
 
 # Syntax 2: using @ Symbol
+
+
 @decorator_function
 def display():
     print('display function Ran')
+
 
 display()
